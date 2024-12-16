8bae5c57c6f2250a214f15352462eae53bb10c2c
Anup sahu <anupsahu.02@gmail.com>
2024-12-15 23:21:44 -1100

    rem vali

--- routes/listing.js
+++ routes/listing.js
@@ -15,14 +15,14 @@ const upload = multer( { storage } );
 
 router.route("/")
 .get(wrapAsync(listingController.index))
-.post( isLoggedIn, upload.single("listing[image]"), validateListing, wrapAsync(listingController.createListing));
+.post( isLoggedIn, upload.single("listing[image]"), wrapAsync(listingController.createListing));
 
 // New Route
 router.get("/new", isLoggedIn, listingController.renderNewForm);
 
 router.route("/:id")
 .get(wrapAsync(listingController.showListing))
-.put(isLoggedIn, isOwner, upload.single("listing[image]"), validateListing, wrapAsync(listingController.updateListing))
+.put(isLoggedIn, isOwner, upload.single("listing[image]"), wrapAsync(listingController.updateListing))
 .delete(isLoggedIn, isOwner,  wrapAsync(listingController.destroyListing));
 
 // Edit Route
